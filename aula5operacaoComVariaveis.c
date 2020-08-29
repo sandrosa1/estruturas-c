c6e492574487a06908ba7ba420d3ff9140939ff4
Sandro Amâncio de Sá <sandro.sa@fatec.sp.gov.br>
2020-08-29 15:58:33 -0300

    Operação com variaveis

---
+++ aula5operacaoComVariaveis.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    //
+    int myAge;
+    int myMother;
+
+    int myDad = 77;
+    myAge = 44;
+    myMother = 68;  
+
+    // %i -> indica onde vai ficar a variavel que está após a virgula
+    printf ("My age is = %i\nMy dad is %i years old\nMy mother is %i years old\n",
+                myAge, myDad,myMother);
+
+    return 0;
+
+}

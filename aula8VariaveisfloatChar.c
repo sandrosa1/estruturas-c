16cacae23cae8e71f885b4b673041f58e3dcd80a
Sandro Amâncio de Sá <sandro.sa@fatec.sp.gov.br>
2020-08-29 16:02:22 -0300

    Variaveis float char

---
+++ aula8VariaveisfloatChar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int inteiro = 5;
+    float decimal = 5.59;
+    char string = 'a';
+
+    printf("%i\n", inteiro );
+    printf("%f\n",decimal);
+    printf("%c\n",string);
+
+    return 0;
+}
